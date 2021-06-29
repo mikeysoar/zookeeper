55e42ba19acc07e046a5ee24604c462316da1ddb
Mikeysoar <mwclimb@gmail.com>
2021-06-29 14:52:58 -0600

    heroku port

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const { animals } = require('./data/animals');
 
 
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-});
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
