16059a0ebfa54629cc5c342335f0d39b3ff37ddb
Mikeysoar <mwclimb@gmail.com>
2021-06-29 15:19:17 -0600

    working heroku

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
-const app = express();
-
 const { animals } = require('./data/animals');
 
-
-
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -49,10 +43,28 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
